b548475242af8143d136ae34fb10159147a9b7e3
andrey.tazov <andrey.tazov@g5e.com>
2021-08-13 10:49:49 +0300

    added gateway service

--- src/test/java/com/example/mental_arithmetic/challenge/ChallengeEventPubTest.java
+++ src/test/java/com/example/mental_arithmetic/challenge/ChallengeEventPubTest.java
@@ -19,7 +19,7 @@ class ChallengeEventPubTest {
     private ChallengeEventPub challengeEventPub;
 
     @Mock
-    AmqpTemplate amqpTemplate;
+    private AmqpTemplate amqpTemplate;
 
     @BeforeEach
     public void setUp() {
@@ -28,23 +28,27 @@ class ChallengeEventPubTest {
 
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    public void sendAttempt(boolean correct) {
+    public void sendsAttempt(boolean correct) {
         // given
         ChallengeAttempt attempt = createTestAttempt(correct);
-        //when
+        // when
         challengeEventPub.challengeSolved(attempt);
         // then
         var exchangeCaptor = ArgumentCaptor.forClass(String.class);
         var routingKeyCaptor = ArgumentCaptor.forClass(String.class);
         var eventCaptor = ArgumentCaptor.forClass(ChallengeSolvedEvent.class);
 
-        verify(amqpTemplate).convertAndSend(exchangeCaptor.capture(),
-                routingKeyCaptor.capture(), eventCaptor.capture());
-        then(exchangeCaptor.getValue()).isEqualTo("attempt." + (correct ? "correct" : "wrong"));
-
+        verify(amqpTemplate).convertAndSend(exchangeCaptor.capture(), routingKeyCaptor.capture(), eventCaptor.capture());
+        then(exchangeCaptor.getValue()).isEqualTo("test.topic");
+        then(routingKeyCaptor.getValue()).isEqualTo("attempt." + (correct ? "correct" : "wrong"));
+        then(eventCaptor.getValue()).isEqualTo(solvedEvent(correct));
     }
 
     private ChallengeAttempt createTestAttempt(boolean correct) {
-        return new ChallengeAttempt(1L, new User(10L, "Jhon"), 30, 40, correct ? 1200 : 1300, correct);
+        return new ChallengeAttempt(1L, new User(10L, "john"), 30, 40, correct ? 1200 : 1300, correct);
+    }
+
+    private ChallengeSolvedEvent solvedEvent(boolean correct) {
+        return new ChallengeSolvedEvent(1L, correct, 30, 40, 10L, "john");
     }
 }
