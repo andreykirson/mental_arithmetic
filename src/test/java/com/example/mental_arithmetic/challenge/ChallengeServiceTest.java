29d28177511527f25a8038da7b90f115ea99d2f5
andrey.tazov <andrey.tazov@g5e.com>
2021-08-25 13:09:23 +0300

    added distributed tracing

--- src/test/java/com/example/mental_arithmetic/challenge/ChallengeServiceTest.java
+++ src/test/java/com/example/mental_arithmetic/challenge/ChallengeServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.amqp.core.AmqpTemplate;
 
 import java.util.Optional;
 
